dd879e8c94466bdc2a647c77535afbd88527e5f0
jiyoung <ji9310@naver.com>
2019-10-02 16:59:40 +0900

    background 추가

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,67 +1,134 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sample_app/helper/login_background.dart';
 
 class AuthPage extends StatelessWidget {
-
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>(); //widget에 key를 부여
+  final GlobalKey<FormState> _formKey =
+      GlobalKey<FormState>(); //widget에 key를 부여
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;  //디바이스 크기 가져오기
+    final Size size = MediaQuery.of(context).size; //디바이스 크기 가져오기
     return Scaffold(
       body: Stack(
         alignment: Alignment.center,
         children: <Widget>[
-          Container(color: Colors.red,), // 배경
+          CustomPaint(
+            size: size,
+            painter: LoginBackground(),
+          ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              Container(margin: EdgeInsets.all(50.0),width: 200, height: 200, color: Colors.amber,), //
+              _logoImage,
               Stack(
                 children: <Widget>[
-                  Form(
-                    key: _formKey,
-                    child: Column(
-                      children: <Widget>[
-                        TextFormField(
-                          controller: _emailController,
-                          decoration: InputDecoration(
-                            icon: Icon(Icons.account_circle),
-                            labelText: 'Email',
-                          ),
-                          validator: (String value) {
-                            if(value.isEmpty) {
-                              return "Please input correct Email.";
-                            }
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          controller: _passwordController,
-                          decoration: InputDecoration(
-                            icon: Icon(Icons.vpn_key),
-                            labelText: 'Password',
-                          ),
-                          validator: (String value) {
-                            if(value.isEmpty) {
-                              return "Please input correct Password.";
-                            }
-                            return null;
-                          },
-                        ),
-                        Text("Forgot Password"),
-                      ],
-                    ),),
-//                      Container(width: 100, height: 50, color: Colors.black,),
-                ],),
-              Container(height: size.height*0.1,),
+                  _inputForm(size),
+                  _authButton(size),
+                ],
+              ),
+              Container(
+                height: size.height * 0.1,
+              ),
               Text('텍스트 들어갈 자리'),
-              Container(height: size.height*0.05,),
+              Container(
+                height: size.height * 0.05,
+              ),
             ],
           ),
         ],
+      ),
+    );
+  }
+
+  Expanded get _logoImage => Expanded(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 40.0, left: 24, right: 24),
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                "https://picsum.photos/200",
+              ),
+            ),
+          ),
+        ),
+      );
+
+  Positioned _authButton(Size size) {
+    return Positioned(
+      left: size.width * 0.15,
+      right: size.width * 0.15,
+      bottom: 0,
+      child: SizedBox(
+        height: 50,
+        child: RaisedButton(
+          color: Colors.blue,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+          child: Text('Login', style: TextStyle(fontSize: 20, color: Colors.white),),
+          onPressed: () {
+            if(_formKey.currentState.validate()) {
+              print(_emailController.text.toString());
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Padding _inputForm(Size size) {
+    return Padding(
+      padding: EdgeInsets.all(size.width * 0.05),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        elevation: 6.0,
+        child: Padding(
+          padding: const EdgeInsets.only(
+              left: 12.0, right: 12.0, top: 12.0, bottom: 32.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.account_circle),
+                    labelText: 'Email',
+                  ),
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return "Please input correct Email.";
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  obscureText: true,
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.vpn_key),
+                    labelText: 'Password',
+                  ),
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return "Please input correct Password.";
+                    }
+                    return null;
+                  },
+                ),
+                Container(
+                  height: 10,
+                ),
+                Text("Forgot Password"),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
